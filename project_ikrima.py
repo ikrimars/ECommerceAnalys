4e780a77fbbe34f2ab855fbfec1586a29a7154b1
ikrimars <106872536+ikrimars@users.noreply.github.com>
2023-12-15 21:30:50 +0700

    Update project_ikrima.py
    2nd change

--- project_ikrima.py
+++ project_ikrima.py
@@ -3,6 +3,15 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
+st.set_page_config(
+    page_title="ECommerceAnalys-Ikrima",
+    page_icon="📊",
+    layout="wide", # atau "centered"
+    initial_sidebar_state="expanded" # atau "collapsed"
+)
+
+# Konten aplikasi Streamlit di sini
+
 st.title('Proyek Analisis Data:E-Commerce Dataset')
 st.header('Nama: Ikrima Rai Saiddah | Email: ikrimarai13@gmail.com | ID Dicoding: Ikrimars')
 st.subheader('Visualization & Explanatory Analysis E-Commerce Dataset')
@@ -52,6 +61,17 @@ agg_pay=order_payments_df[['payment_type','order_id']]
 agg_pay=order_payments_df.groupby('payment_type')['order_id'].agg('count').reset_index()
 agg_pay.columns = ['payment type','count order']
 
+#clean order_review
+datetime_review = ["review_creation_date","review_answer_timestamp"]
+
+for column in datetime_review:
+  order_review_df[column] = pd.to_datetime(order_review_df[column])
+
+datetime_item = ["shipping_limit_date"]
+
+for column in datetime_item:
+  order_item_df[column] = pd.to_datetime(order_item_df[column])
+
 # Analyze delivery time
 order_df['order_purchase_timestamp'] = pd.to_datetime(order_df['order_purchase_timestamp'])  # Convert to datetime
 order_df['order_estimated_delivery_date'] = pd.to_datetime(order_df['order_estimated_delivery_date'])  # Convert to datetime
@@ -76,6 +96,12 @@ filter_max = order_df['waktu_pengiriman'] > max_value
 
 outlier = order_df[filter_min | filter_max]
 clr_outlier = order_df[~(filter_min | filter_max)]
+
+#jika ngin hari saja dengan formt int
+clr_outlier['waktu_pengiriman'] = (order_df['order_estimated_delivery_date'] - order_df['order_purchase_timestamp']).dt.days.astype('int')
+
+# Menghitung rata-rata waktu pengiriman
+rata_pengiriman = clr_outlier['waktu_pengiriman'].mean()
 
 # Aggregate data for plotting
 agg_clr_outlier = clr_outlier[['order_purchase_timestamp', 'order_id']]
@@ -111,10 +137,22 @@ st.write(order_df.head())
 
 # Display the delivery time analysis
 st.subheader("Delivery Time Analysis")
-st.write(f"Average delivery time: {rata_rata_pengiriman:.2f} days")
+st.write(f"Average delivery time: {rata_pengiriman:.2f} days")
 st.write(f"Minimum delivery time: {min_value} days")
 st.write(f"Maximum delivery time: {max_value} days")
 
+st.header("Jumlah Skor Ulasan ")
+# Display the orders DataFrame
+st.subheader("Order Review Dataset")
+st.write(order_review_df.head())
+st.subheader("Review Score")
+review= order_review_df.groupby(by='review_score').agg({
+ "review_score":["count"]
+})
+st.write(review)
+st.markdown('Pada review score terdapat 57328 dengan score 5 dan paling sedikit dengan score 2 sebanyak 3151 review')
+
+st.header("Total Revenue per Month and Year")
 # Create tabs
 tabs = st.tabs(["Total Revenue per Month", "Total Revenue per Month and Year"])
 
@@ -148,11 +186,7 @@ with tabs[1]:
     plt.xticks(fontsize=10)
     plt.yticks(fontsize=10)
     st.pyplot(plt)
-    st.markdown('Terdapat 10 produk paling mahal dapat dilihat pada gambar di atas ini. Pada 10 produk tersebut terdapat produk yang paling mahal yaitu utilidades domesticas sebesar 6735.0 USD')
-
-st.header("Distribusi tipe pembayaran untuk order")
-# st.image("Distribusipayment.jpg")
-st.markdown('Metode pembayaran dengan menggunakan credi card banyak diminati customers untuk melakukan order dan paling sedikit menggunakan debit card.')
+    st.markdown("tahun 2017 bulan november memiliki jumlah order tertinggi sebanyak 7499 kali")
 
 item_category_df = pd.merge(
     left=order_item_df,
@@ -164,15 +198,16 @@ item_category_df = pd.merge(
 
 st.write(item_category_df.head())
 
+st.header("Analisa Kategori Produk")
 #kolom data kaegori
 col1, col2, col3 = st.columns(3)
-# col1, col2, col3 = st.columns([2, 1, 1])
-
 
 # Mengelompokkan data berdasarkan kategori produk dan menghitung jumlah pembelian
 top_categories = item_category_df['product_category_name'].value_counts().nlargest(10)
+
 with col1:
     # Membuat grafik batang horizontal di Streamlit
+    st.subheader("Kategori Produk Paling Banyak Dibeli")
     fig, ax = plt.subplots()
     ax.barh(top_categories.index, top_categories.values, color='blue')
     ax.set_xlabel('Jumlah Pembelian')
@@ -185,10 +220,12 @@ with col1:
 
     # Menampilkan grafik di Streamlit
     st.pyplot(fig)
+    st.markdown("cama_mesa_banho memiliki total order sebanyak 12718, dan memiliki harga tertinggi sebesar 3980.0 USD.")
 
 # Mengelompokkan data berdasarkan produk dan mencari harga tertinggi
 products_expensive = item_category_df.groupby('product_category_name')['price'].max().nlargest(10)
 with col2:
+    st.subheader("Kategori Produk dengan Harga Tertinggi")
     # Membuat grafik bar horizontal di Streamlit
     fig, ax = plt.subplots()
     ax.barh(products_expensive.index, products_expensive.values, color='red')
@@ -202,11 +239,13 @@ with col2:
 
     # Menampilkan grafik di Streamlit
     st.pyplot(fig)
+    st.markdown('10 produk paling mahal yaitu utilidades domesticas sebesar 6735.0 USD')
 
 # Mengelompokkan data berdasarkan kategori produk dan mencari ongkos kirim tertinggi
 ongkir_graph = item_category_df.groupby('product_category_name')['freight_value'].max().nlargest(10)
 
 with col3:
+    st.subheader("Kategori Produk dengan Ongkir Tertinggi")
     fig, ax = plt.subplots()
     ax.bar(ongkir_graph.index, ongkir_graph.values, color='orange')
     ax.set_xlabel('Kategori Produk')
@@ -220,8 +259,10 @@ with col3:
 
     # Menampilkan grafik di Streamlit
     st.pyplot(fig)
+    st.markdown("produk dengan ongkos kirim tertinggi yaitu utilidades bebes sebesar 409.68 USD")
 
 # Membuat gambar dengan ukuran lebih kecil
+st.header("Distribusi tipe pembayaran untuk order")
 fig, ax = plt.subplots(figsize=(8, 4))
 ax.set_title('Distribution of Payment Types for Number of Orders')
 
@@ -238,13 +279,19 @@ with st.container():
         colors=colors,
         explode=explode,
         shadow=True,
-        startangle=75
+        startangle=75,
+        textprops = dict(size=7)  # Atur ukuran teks di sini
     )
 
     # Menampilkan diagram lingkaran di Streamlit
     st.pyplot(fig, clear_figure=True)
 
+st.markdown('Metode pembayaran dengan menggunakan credi card banyak diminati customers untuk melakukan order dan paling sedikit menggunakan debit card.')
+
 st.header("Lokasi Geografis Persebaran Pelanggan")
+# Display the orders DataFrame
+st.subheader("Geolocation Dataset")
+st.write(geolocation_df.head())
 # Tentukan warna berdasarkan nilai longitude dan latitude
 colors = geolocation_df['geolocation_lng']
 
@@ -254,3 +301,20 @@ plt.ylabel('Latitude')
 plt.title('Lokasi Geografis Pelanggan')
 st.pyplot(plt)
 st.markdown('Berdasarkan data diatas, Setiap titik akan memiliki koordinat longitude di sumbu x dan latitude di sumbu y. gambaran visual tentang sebaran lokasi pelanggan di berbagai daerah. hasilnya berkumpul pada satu titik lokasi geografis dengan rata-rata latitude -20.998353 dan longitude -46.461098. Semakin gelap warnanya, semakin rendah nilai longitude. Hal ini memungkinkan kita untuk melihat bagaimana pelanggan terdistribusi secara horizontal (sepanjang garis bujur)')
+st.header("Dataset Informasi Kondisi Demografi")
+order_customers_df = pd.merge(
+    left=order_df,
+    right=customers_df,
+    how="left",
+    left_on="customer_id",
+    right_on="customer_id"
+)
+st.write(order_customers_df.head())
+st.subheader("informasi Demografi Pelanggan")
+colom1, colom2 = st.columns(2)
+with colom1:
+    st.write(order_customers_df.groupby(by="customer_city").order_id.nunique().sort_values(ascending=False).head(10))
+with colom2:
+    st.write(order_customers_df.groupby(by="customer_state").order_id.nunique().sort_values(ascending=False))
+
+st.markdown("informasi mengenai pelanggan sebanyak 99441 orang yang menunjukkan terdapat 4119 kota yang berasal dari 27 negara. data tersebut, menunjukkan customers paling banyak berasal dari kota sao paulo dengan jumlah 15540 orang dengan kode negara SP sebanyak 41746 orang dari total pelanggan 99441 orang.")
